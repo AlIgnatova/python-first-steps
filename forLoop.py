b3d518a95998238e5d68a297c922f3c5c067af20
AlIgnatova <86323002+AlIgnatova@users.noreply.github.com>
2025-01-22 17:21:52 +0200

    Add files via upload

---
+++ forLoop.py
@@ -0,0 +1,20 @@
+for i in range (5):
+    print('Jimmy Five Times ' + str(i))
+
+total = 0
+
+for num in range(101):
+    total+=num
+
+print(total)
+
+for i in range (12,16):
+    print(i)
+
+for i in range (1, 12, 2):
+    print(i)
+
+print('new line')
+
+for i in range (20, -2, -1):
+    print(i)
---
+++ functions2.py
@@ -0,0 +1,27 @@
+print('Hello', end='')
+print('World')
+print('cat','dog','mouse')
+print('cat','dog','mouse', sep='ABC')
+
+
+def hello():
+    print('Howdy!')
+    print('Howdy!!!')
+    print('Hello there!')
+
+hello()
+
+
+def hiyou (name):
+    print('Hello, ' + name)
+
+hiyou(input())
+
+
+def plusOne(number):
+    return number +1
+
+print(plusOne(5))
+
+
+
